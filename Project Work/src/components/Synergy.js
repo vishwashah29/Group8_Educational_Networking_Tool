5cecec3962f7232e96f974869915393620b6c296
Nimish Vithalani <nimish.v@somaiya.edu>
2021-04-18 22:02:02 +0530

    login page

--- Project Work/src/components/Synergy.js
+++ Project Work/src/components/Synergy.js
@@ -3,21 +3,23 @@ import background1 from "../Images/logo.png";
 const Synergy = () => {
     return (
         <div>
-        
+        <center>
             <div className='syn'
               style={{ 
                 backgroundImage: `url(${background1})` ,
-                 height :'500px',
-                 width : '450px',
+                 height :'200px',
+                 width : '300px',
         
                 }}>
             </div>
+            </center>
+
             <div>
-              <h1 className='textcls'>Welcome to </h1>
-                <h1 className='textcls'>the community</h1>
+
+             <center> <h1 className='textcls'>Welcome to the community </h1>
+                <h1 className='textcls'></h1></center>
                 
             </div>
-
         </div>
     );
     
